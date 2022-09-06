a65a46f318eab979c266d928fb0f0bb309d57fa6
Luke-Longo <lukelongo0421@gmail.com>
2022-09-06 09:03:26 -0400

    works so far

--- server/api/incoming-message.ts
+++ server/api/incoming-message.ts
@@ -12,24 +12,67 @@ export default defineEventHandler(async (event) => {
 
 	const body: TwilioIncoming = await useBody(event);
 
+	let user_id;
 	try {
-		let user_id;
-		try {
-			const { data } = await supabase
-				.from("profiles")
-				.select("*")
-				.contains("phoneNumbers", [body.to]);
-			user_id = data[0].user_id;
-		} catch (error) {
-			console.log(error);
-		}
+		const { data } = await supabase
+			.from("profiles")
+			.select("*")
+			.contains("phoneNumbers", [body.to]);
+		user_id = data[0].user_id;
+	} catch (error) {
+		console.log(error);
+	}
 
-		// will need to clean this up to use user_id's if making public app
-		const { data: lead_ids, error: err } = await supabase
+	// will need to clean this up to use user_id's if making public app
+	let lead_ids;
+	try {
+		const { data, error } = await supabase
 			.from("leads")
 			.select("lead_id")
+			.eq("user_id", user_id)
 			.contains("wireless", [body.from]);
 
+		if (error) {
+			throw error;
+		}
+		lead_ids = data.map((lead) => lead.lead_id);
+	} catch (error) {
+		console.log(error);
+	}
+
+	if (lead_ids.length > 1) {
+		console.log("more than one lead found with that number");
+
+		let counter = 0;
+
+		lead_ids.forEach(async (lead_id) => {
+			let incoming_message: IncomingMessage = {
+				user_id: user_id,
+				message: body.body,
+				from: body.from,
+				to: body.to,
+				sid: body.sid + `_${counter}`,
+				created_at: body.date_created,
+				sent_at: body.date_sent,
+				updated_at: body.date_updated,
+				status: body.status,
+				direction: body.direction,
+				error_code: body.error_code,
+				error_message: body.error_message,
+				lead_id: lead_id,
+			};
+
+			counter++;
+
+			try {
+				const { error } = await supabase
+					.from("incoming_messages")
+					.insert(incoming_message);
+			} catch (error) {
+				console.log(error);
+			}
+		});
+	} else {
 		let incoming_message: IncomingMessage = {
 			user_id: user_id,
 			message: body.body,
@@ -43,20 +86,21 @@ export default defineEventHandler(async (event) => {
 			direction: body.direction,
 			error_code: body.error_code,
 			error_message: body.error_message,
-			lead_id: lead_ids[0].lead_id,
+			lead_id: lead_ids[0],
 		};
 
-		const { error } = await supabase
-			.from("incoming_messages")
-			.insert(incoming_message);
+		try {
+			const { error } = await supabase
+				.from("incoming_messages")
+				.insert(incoming_message);
 
-		if (error || err) {
-			throw error || err;
+			if (error) {
+				throw error;
+			}
+		} catch (error) {
+			console.log(error);
 		}
-	} catch (error) {
-		console.log(error);
 	}
-
 	// perform logic based on what the event is (e.g. message sent, message received, etc.)
 
 	const { MessagingResponse } = twilio.twiml;
