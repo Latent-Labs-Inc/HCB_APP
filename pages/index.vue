e0373edfb3b6bf7d7d9455658b6db030849e5698
Luke-Longo <lukelongo0421@gmail.com>
2022-09-09 13:04:18 -0400

    get there

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,12 @@
 <template>
 	<div>
 		<h3 class="header">Home</h3>
-
+		<Bar
+			:chart-options="chartOptions"
+			:chart-data="chartData"
+			:chart-id="chartId"
+			:dataset-id-key="datasetIdKey"
+		/>
 		<!-- <UiTable
 			:cols="cols"
 			:gridCols="gridCols"
@@ -15,6 +20,40 @@
 </template>
 
 <script setup lang="ts">
+import { Bar } from "vue-chartjs";
+import {
+	Chart as ChartJS,
+	Title,
+	Tooltip,
+	Legend,
+	BarElement,
+	CategoryScale,
+	LinearScale,
+} from "chart.js";
+
+ChartJS.register(
+	Title,
+	Tooltip,
+	Legend,
+	BarElement,
+	CategoryScale,
+	LinearScale
+);
+
+const chartOptions = {
+	responsive: true,
+	maintainAspectRatio: false,
+};
+
+const chartData = {
+	labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+	datasets: [{ data: [10, 20, 30, 40, 50, 60, 70, 80, 90, 100] }],
+};
+
+const chartId = "myChart";
+
+const datasetIdKey = "id";
+
 await useLoadContent();
 
 const cols = ref(["Name", "Email", "Phone", "Order", "Patient"]);
