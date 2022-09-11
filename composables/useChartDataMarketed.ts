5433bc87924a9a2c14cf4b0921abacc678d4c47c
Luke-Longo <lukelongo0421@gmail.com>
2022-09-11 18:42:32 -0400

    time to make money

---
+++ composables/useChartDataMarketed.ts
@@ -0,0 +1,58 @@
+export default async function useChartData() {
+	const { $supabase } = useNuxtApp();
+
+	const { data: fiverrMarketed } = await $supabase
+		.from("leads")
+		.select("*")
+		.eq("texted", true)
+		.eq("leadType", "fiverr");
+
+	const { data: propStreamMarketed } = await $supabase
+		.from("leads")
+		.select("*")
+		.eq("texted", true)
+		.eq("leadType", "propStream");
+
+	const { data: foreclosureDailyMarketed } = await $supabase
+		.from("leads")
+		.select("*")
+		.eq("texted", true)
+		.eq("leadType", "foreclosureDaily");
+
+	const { data: otherMarketed } = await $supabase
+		.from("leads")
+		.select("*")
+		.eq("texted", true)
+		.eq("leadType", "other");
+
+	const chartOptions = {
+		responsive: true,
+		maintainAspectRatio: false,
+	};
+
+	const chartData = {
+		labels: ["Fiverr", "ForeclosureDaily", "PropStream", "Other"],
+		datasets: [
+			{
+				label: "Total Leads Marketed",
+				data: [
+					fiverrMarketed?.length,
+					foreclosureDailyMarketed?.length,
+					propStreamMarketed?.length,
+					otherMarketed?.length,
+				],
+				backgroundColor: [
+					"rgba(2, 173, 36, .9)",
+					"rgba(2, 173, 36, 0.9)",
+					"rgba(2, 173, 36, 0.9)",
+					"rgba(2, 173, 36, 0.9)",
+				],
+			},
+		],
+	};
+
+	return {
+		chartData,
+		chartOptions,
+	};
+}
--- pages/index.vue
+++ pages/index.vue
@@ -22,57 +22,9 @@
 <script setup lang="ts">
 await useLoadContent();
 
-const chartOptions = {
-	responsive: true,
-	maintainAspectRatio: false,
-};
-
 const { $supabase } = useNuxtApp();
 
-const { data: fiverrLeads } = await $supabase
-	.from("leads")
-	.select("*")
-	.eq("texted", true)
-	.eq("leadType", "fiverr");
-
-const { data: propStreamLeads } = await $supabase
-	.from("leads")
-	.select("*")
-	.eq("texted", true)
-	.eq("leadType", "propStream");
-
-const { data: foreclosureDailyLeads } = await $supabase
-	.from("leads")
-	.select("*")
-	.eq("texted", true)
-	.eq("leadType", "foreclosureDaily");
-
-const { data: otherLeads } = await $supabase
-	.from("leads")
-	.select("*")
-	.eq("texted", true)
-	.eq("leadType", "other");
-
-const chartData = {
-	labels: ["Fiverr", "ForeclosureDaily", "PropStream", "Other"],
-	datasets: [
-		{
-			label: "Total Leads Marketed",
-			data: [
-				fiverrLeads?.length,
-				foreclosureDailyLeads?.length || 15,
-				propStreamLeads?.length,
-				otherLeads?.length,
-			],
-			backgroundColor: [
-				"rgba(2, 173, 36, .9)",
-				"rgba(2, 173, 36, 0.9)",
-				"rgba(2, 173, 36, 0.9)",
-				"rgba(2, 173, 36, 0.9)",
-			],
-		},
-	],
-};
+const { chartData, chartOptions } = await useChartData();
 
 const datasetIdKey = "Leads";
 
