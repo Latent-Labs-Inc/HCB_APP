0b6515e10bb994a12034cb66928e023817ea61fa
Latent-Labs <luke@latent-labs.com>
2023-02-03 12:02:05 -0500

    updated the texting to be much cleaner

--- pages/messages/send-bulk/index.vue
+++ pages/messages/send-bulk/index.vue
@@ -14,26 +14,75 @@
 					:row="true"
 				/>
 				<transition name="fade" mode="out-in">
-					<textarea v-model.trim="message" v-if="!radios[2].selected" />
-					<div class="flex my-auto gap-4 justify-center" v-else>
-						<label class="my-auto" for="">Search</label>
-						<input class="py-0" type="text" />
-						<p
-							class="my-auto px-4 py-2 dark:bg-darkBg bg-primary rounded-lg cursor-pointer dark:hover:bg-darkPrimary hover:bg-primary trans"
-							@click="searchLeads"
-						>
-							Search
-						</p>
+					<div class="grid gap-4" v-if="!templateSelected">
+						<transition name="fade" mode="out-in">
+							<textarea v-model.trim="message" v-if="!radios[2].selected" />
+							<div class="flex my-auto gap-4 justify-center" v-else>
+								<label class="my-auto" for="">Search</label>
+								<input class="py-0" type="text" v-model.trim="templateInput" />
+								<p
+									class="my-auto px-4 py-2 dark:bg-darkBg bg-primary rounded-lg cursor-pointer dark:hover:bg-darkPrimary hover:bg-primary trans"
+									@click="searchTemplates"
+								>
+									Search
+								</p>
+							</div>
+						</transition>
+					</div>
+					<div class="grid gap-2" v-else>
+						<p class="mx-auto">Selected Message</p>
+						<p class="my-auto">{{ templateSelected.message }}</p>
 					</div>
 				</transition>
-				<button class="mx-auto reverse">Preview Leads</button>
+				<div class="flex">
+					<button
+						class="mx-auto reverse mt-4 bg-primary text-white hover:text-primary hover:bg-white dark:bg-darkBg border-none"
+						@click="searchLeads('search')"
+					>
+						{{ leads.length > 0 ? 'Search New' : 'Search Leads' }}
+					</button>
+					<button
+						v-if="leads.length > 0"
+						class="mx-auto reverse mt-4 border-none bg-darkSecondary dark:bg-darkPrimary hover:dark:bg-black"
+						@click="searchLeads('toggle')"
+					>
+						{{ showLeads ? 'Hide Leads' : 'Show Leads' }}
+					</button>
+				</div>
+				<transition name="fade" mode="out-in">
+					<UiBaseList
+						v-if="templates.length > 0"
+						:data="templates"
+						:gridCols="'grid-cols-2'"
+						:colKeyPairs="templateColKeyPairs"
+						:showAdd="false"
+						:itemName="'Template'"
+						:flip="true"
+						@selected="handleSelected('template', $event)"
+					/>
+				</transition>
+				<transition name="fade" mode="out-in">
+					<UiBaseList
+						v-if="showLeads"
+						:data="leads"
+						:gridCols="'grid-cols-5'"
+						:colKeyPairs="leadColKeyPairs"
+						:showAdd="false"
+						:itemName="'Lead'"
+						:flip="true"
+						@selected="handleSelected('lead', $event)"
+					/>
+				</transition>
 			</div>
 		</transition>
+		<div class="flex justify-center mt-4 mb-24">
+			<button @click="sendTexts" class="border-none">Send Bulk Messages</button>
+		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
-import { Template } from '~~/types/types';
+import { Template, Lead } from '~~/types/types';
 import { Database } from '~/types/supabase';
 import { useUiStore } from '~/stores/ui';
 import { useAuthStore } from '~~/stores/auth';
@@ -44,16 +93,26 @@ const router = useRouter();
 const client = useSupabaseClient<Database>();
 
 const message = ref('');
-
 const templateSelected = ref(null as Template | null);
+const templateInput = ref('');
+const templates = ref([] as Template[]);
 
 const sentMessages = ref<number>(0);
 
-const handleMessage = async () => {};
+const leads = ref([] as Lead[]);
+const showLeads = ref(false);
 
-const handleContinue = () => {
-	sentMessages.value = 0;
-};
+const leadColKeyPairs = reactive({
+	'Lead Provider': 'leadProvider',
+	'Lead Type': 'leadType',
+	Address: 'propertyAddress.address1',
+	City: 'propertyAddress.city',
+	'Phone Numbers': 'wireless',
+});
+const templateColKeyPairs = reactive({
+	Name: 'name',
+	Message: 'message',
+});
 
 const radios = ref([
 	{
@@ -76,7 +135,7 @@ const radios = ref([
 			{ value: 'all', label: 'All' },
 			{ value: 'probate', label: 'Probate' },
 			{ value: 'foreclosure', label: 'Foreclosure' },
-			{ value: 'eviction', label: 'Evictions' },
+			{ value: 'evictions', label: 'Evictions' },
 			{ value: 'other', label: 'Other' },
 		],
 	},
@@ -92,30 +151,86 @@ const radios = ref([
 ]);
 
 const handleOption = (index: number, value: string | boolean) => {
-	console.log(index, value);
 	radios.value[index].selected = value as string;
-};
-
-const handleSelected = (template: Template) => {
-	templateSelected.value = template;
-};
-
-const handleChange = async () => {
-	templateSelected.value = null;
-};
-
-const searchLeads = async () => {
-	let query = client.from('leads').select('*');
-
-	if (radios.value[0].selected !== 'all') {
-		query = query.eq('leadProvider', radios.value[0].selected);
+	if (index === 2) {
+		if (value === false) {
+			templateSelected.value = null;
+		}
 	}
-	if (radios.value[1].selected !== 'all') {
-		query = query.eq('leadType', radios.value[0].selected);
+};
+
+const handleSelected = (type: 'template' | 'lead', item: Template | Lead) => {
+	if (type === 'template') {
+		templateSelected.value = item as Template;
+		templates.value = [];
 	}
+};
 
-	const { data, error } = await query;
+const searchTemplates = async () => {
+	const { data, error } = await client
+		.from('templates')
+		.select('*')
+		.ilike('name', `%${templateInput.value}%`);
+	if (error) {
+		console.log(error);
+		return;
+	}
+	console.log(data);
+	// @ts-ignore
+	templates.value = data;
+};
 
-	console.log(data, error);
+const searchLeads = async (action: 'search' | 'toggle') => {
+	if (action === 'toggle') {
+		showLeads.value = !showLeads.value;
+		return;
+	} else {
+		let query = client.from('leads').select('*').eq('texted', false);
+
+		if (radios.value[0].selected !== 'all') {
+			query = query.eq('leadProvider', radios.value[0].selected);
+		}
+		if (radios.value[1].selected !== 'all') {
+			query = query.eq('leadType', radios.value[1].selected);
+		}
+
+		const { data, error } = await query;
+		if (error) {
+			console.log(error);
+			return;
+		}
+		console.log(data);
+		// @ts-ignore
+		leads.value = data;
+		showLeads.value = true;
+	}
+};
+
+const sendTexts = async () => {
+	if (leads.value.length === 0) {
+		alert('Please select leads to send messages to');
+		return;
+	}
+	if (templateSelected.value && !templateSelected.value.message) {
+		alert('Please select a template with a message');
+		return;
+	}
+	uiStore.toggleFunctionLoading(true);
+	try {
+		const { data, error } = await $fetch('/api/bulk-send-messages', {
+			method: 'POST',
+			body: {
+				leads: leads.value,
+				message: templateSelected.value
+					? templateSelected.value.message
+					: message.value,
+				user_id: leads.value[0].user_id,
+			},
+		});
+	} catch (error) {
+		console.log(error);
+	} finally {
+		uiStore.toggleFunctionLoading(false);
+	}
 };
 </script>
---
+++ server/api/bulk-send-messages.post.ts
@@ -0,0 +1,53 @@
+import twilio from 'twilio';
+
+import { serverSupabaseClient } from '#supabase/server';
+import { Lead, TwilioResponse } from '~~/types/types';
+
+export default defineEventHandler(async (event) => {
+	const { leads, user_id, message } = await readBody(event);
+	const supabase = serverSupabaseClient(event);
+	const config = useRuntimeConfig();
+
+	const accountSid = config.private.TWILIO_ACCOUNT_SID;
+	const authToken = config.private.TWILIO_AUTH_TOKEN;
+	const twilioNumber = config.private.TWILIO_PHONE_NUMBER;
+
+	const client = twilio(accountSid, authToken);
+
+	let error = null as Error | null;
+
+	let messageCounter = 0;
+	leads.forEach(async (lead: Lead) => {
+		lead.wireless.forEach(async (wireless: string) => {
+			try {
+				const twilioResponse = await client.messages.create({
+					body: message,
+					from: twilioNumber,
+					to: wireless,
+				});
+				if (twilioResponse.errorMessage)
+					throw new Error(twilioResponse.errorMessage);
+				console.log(twilioResponse);
+				messageCounter++;
+			} catch (error) {
+				console.log(error);
+				error = error;
+			}
+		});
+
+		try {
+			await supabase
+				.from('leads')
+				.update({ texted: true })
+				.eq('lead_id', lead.lead_id);
+		} catch (error) {
+			console.log(error);
+			error = error;
+		}
+	});
+
+	return {
+		data: messageCounter,
+		error,
+	};
+});
