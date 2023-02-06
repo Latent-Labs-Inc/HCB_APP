3ccec6b5966d2d663eccb620dae863b9002b6949
Latent-Labs <luke@latent-labs.com>
2023-02-06 14:40:53 -0500

    try a different route

--- server/api/send-message-single.post.ts
+++ server/api/send-message-single.post.ts
@@ -1,18 +1,21 @@
 import twilio from 'twilio';
 import { serverSupabaseServiceRole } from '#supabase/server';
 import { Database } from '~/types/supabase';
+import { createClient } from '@supabase/supabase-js';
 
 export default defineEventHandler(async (event) => {
+	const config = useRuntimeConfig();
+
 	const { phone, message } = (await readBody(event)) as {
 		phone: string;
 		message: string;
 	};
 
-	const supabase = serverSupabaseServiceRole<Database>(event);
+	const supabase = createClient(
+		config.public.SUPABASE_URL,
+		config.private.SUPABASE_SERVICE_KEY
+	);
 
-	console.log('Sending message to', phone);
-
-	const config = useRuntimeConfig();
 	const accountSid = config.private.TWILIO_ACCOUNT_SID;
 	const authToken = config.private.TWILIO_AUTH_TOKEN;
 	const twilioNumber = config.private.TWILIO_PHONE_NUMBER;
