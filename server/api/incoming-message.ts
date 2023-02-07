01104737a2b90b946ea3453659733d44f2e5e7c6
Latent-Labs <luke@latent-labs.com>
2023-02-07 10:00:58 -0500

    try it now

--- server/api/incoming-message.ts
+++ server/api/incoming-message.ts
@@ -11,7 +11,6 @@ export default defineEventHandler(async (event) => {
 
 	const accountSid = config.private.TWILIO_ACCOUNT_SID;
 	if (body.AccountSid !== accountSid) {
-		console.log('Invalid Twilio AccountSid');
 		return {
 			data: null,
 			message: 'Invalid Twilio AccountSid',
@@ -22,6 +21,8 @@ export default defineEventHandler(async (event) => {
 	const client = twilio(accountSid, authToken);
 
 	const contactPhoneNumbers = ['+18134084221'];
+
+	let supabaseError: Error | null = null;
 
 	contactPhoneNumbers.forEach(async (number) => {
 		let regex = /stop|no|harassment|fuck/gi;
@@ -36,82 +37,74 @@ export default defineEventHandler(async (event) => {
 
 	try {
 		// getting user id from phone number
-		const { data, error } = await supabase
+		const { data, error: err } = await supabase
 			.from('profiles')
 			.select('*')
 			.contains('phoneNumbers', [body.To])
 			.single();
-		if (error) throw error;
+		if (err) throw err;
 
 		const user_id = data.user_id;
-		if (user_id) {
-			try {
-				const { data: leads, error } = await supabase
-					.from('leads')
-					.select('*')
-					.eq('user_id', user_id)
-					.contains('wireless', [body.From]);
-				if (error || !leads) {
-					throw error;
-				}
-
-				console.log(leads);
-
-				leads.forEach(async (lead) => {
-					const propertyAddress = !!lead.propertyAddress
-						? lead.propertyAddress
-						: { address1: 'None Found', city: '', state: '', zip: '' };
-
-					let incoming_message = {
-						user_id: user_id!,
-						message: body.Body,
-						from: body.From,
-						to: body.To,
-						sid: body.MessageSid,
-						created_at: new Date().toISOString(),
-						sent_at: new Date().toISOString(),
-						updated_at: new Date().toISOString(),
-						status: body.MessageStatus,
-						direction: 'inbound',
-						errorCode: '',
-						errorMessage: '',
-						lead_id: lead.lead_id,
-						propertyAddress: propertyAddress as unknown as Json,
-					};
-
-					try {
-						const { error } = await supabase
-							.from('incoming_messages')
-							.insert(incoming_message);
-						if (error) {
-							throw error;
-						}
-					} catch (error) {
-						console.log(error);
-					}
-				});
-				return {
-					statusCode: 200,
-					body: 'success',
-				};
-			} catch (error) {
-				console.log(error);
-				return {
-					statusCode: 500,
-					body: error,
-				};
-			}
-		} else {
+		if (!user_id)
 			return {
 				statusCode: 500,
 				body: 'user not found',
 			};
+
+		const { data: leads, error } = await supabase
+			.from('leads')
+			.select('*')
+			.eq('user_id', user_id)
+			.contains('wireless', [body.From]);
+		if (error || !leads) {
+			throw error || 'No leads found';
 		}
+
+		leads.forEach(async (lead) => {
+			const propertyAddress = !!lead.propertyAddress
+				? lead.propertyAddress
+				: { address1: 'None Found', city: '', state: '', zip: '' };
+
+			let incoming_message = {
+				user_id: user_id!,
+				message: body.Body,
+				from: body.From,
+				to: body.To,
+				sid: body.MessageSid,
+				created_at: new Date().toISOString(),
+				sent_at: new Date().toISOString(),
+				updated_at: new Date().toISOString(),
+				status: body.MessageStatus,
+				direction: 'inbound',
+				errorCode: '',
+				errorMessage: '',
+				lead_id: lead.lead_id,
+				propertyAddress: propertyAddress as unknown as Json,
+			};
+
+			try {
+				const { error } = await supabase
+					.from('incoming_messages')
+					.insert(incoming_message);
+				if (error) {
+					throw error;
+				}
+			} catch (error) {
+				supabaseError = error as Error;
+				console.log(error);
+			}
+		});
+
+		return {
+			statusCode: 200,
+			body: 'success',
+			supabaseError,
+		};
 	} catch (error) {
-		console.log(error);
 		return {
 			statusCode: 500,
 			body: error,
+			supabaseError,
 		};
 	}
 });
