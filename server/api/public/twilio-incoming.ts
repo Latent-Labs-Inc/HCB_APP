66c84c1f5692454ab975ac726f2d750e473fd171
Latent-Labs <luke@latent-labs.com>
2023-02-06 19:58:18 -0500

    take this big boi

---
+++ server/api/public/twilio-incoming.ts
@@ -0,0 +1,25 @@
+import { TwilioIncoming } from '~~/types/types';
+
+export default defineEventHandler(async (event) => {
+	const body: TwilioIncoming = await readBody(event);
+	const config = useRuntimeConfig();
+
+	if (body.AccountSid === config.private.TWILIO_ACCOUNT_SID) {
+		await $fetch('/api/incoming-message', {
+			method: 'POST',
+			body,
+		});
+		return {
+			data: null,
+			message: 'OK',
+			error: null,
+		};
+	} else {
+		console.log('Invalid Twilio AccountSid');
+		return {
+			data: null,
+			message: 'Invalid Twilio AccountSid',
+			error: 'Invalid Twilio AccountSid',
+		};
+	}
+});
