32d6a14392078c3b90ee2cc75b5d88620e76b9ea
Latent-Labs <luke@latent-labs.com>
2023-02-07 14:47:07 -0500

    publish changes

--- server/api/incoming-message.ts
+++ server/api/incoming-message.ts
@@ -46,66 +46,91 @@ export default defineEventHandler(async (event) => {
 		if (err) throw err;
 
 		const user_id = data.user_id;
+
 		if (!user_id)
 			return {
 				statusCode: 500,
 				body: 'user not found',
 			};
 
-		const { data: leads, error } = await supabase
+		const { data: lead, error } = await supabase
 			.from('leads')
 			.select('*')
 			.eq('user_id', user_id)
-			.contains('wireless', [body.From]);
-		if (error || !leads) {
+			.contains('wireless', [body.From])
+			.single();
+		if (error || !lead) {
 			throw error || 'No leads found';
 		}
 
-		leads.forEach(async (lead) => {
-			const propertyAddress = !!lead.propertyAddress
-				? lead.propertyAddress
-				: { address1: 'None Found', city: '', state: '', zip: '' };
+		const propertyAddress = !!lead.propertyAddress
+			? lead.propertyAddress
+			: { address1: 'None Found', city: '', state: '', zip: '' };
 
-			let incoming_message = {
-				user_id: user_id,
-				message: body.Body,
-				from: body.From,
-				to: body.To,
-				sid: body.MessageSid,
-				created_at: new Date().toISOString(),
-				sent_at: new Date().toISOString(),
-				updated_at: new Date().toISOString(),
-				status: body.MessageStatus,
-				direction: 'inbound',
-				errorCode: '',
-				errorMessage: '',
-				lead_id: lead.lead_id,
-				propertyAddress: propertyAddress as unknown as Json,
-			};
+		let incoming_message = {
+			user_id: user_id,
+			message: body.Body,
+			from: body.From,
+			to: body.To,
+			sid: body.MessageSid,
+			created_at: new Date().toISOString(),
+			sent_at: new Date().toISOString(),
+			updated_at: new Date().toISOString(),
+			status: body.MessageStatus,
+			direction: 'inbound',
+			errorCode: '',
+			errorMessage: '',
+			lead_id: lead.lead_id,
+			propertyAddress: propertyAddress as unknown as Json,
+		};
 
-			try {
-				const { data, error } = await supabase
-					.from('incoming_messages')
-					.insert(incoming_message)
-					.returns();
+		try {
+			const { data, error } = await supabase
+				.from('incoming_messages')
+				.insert(incoming_message)
+				.returns();
 
-				if (error) {
-					throw error;
-				}
-				insertedMessage = data[0] ? 'success' : 'failed';
-				supabaseError = error;
-			} catch (error) {
-				supabaseError = error as Error;
-				console.log(error);
+			if (error) {
+				throw error;
 			}
-		});
+			insertedMessage = data[0] ? 'success' : 'failed';
+			supabaseError = error;
+		} catch (error) {
+			supabaseError = error as Error;
+			console.log(error);
+		}
+
+		// leads.forEach(async (lead) => {
+		// 	const propertyAddress = !!lead.propertyAddress
+		// 		? lead.propertyAddress
+		// 		: { address1: 'None Found', city: '', state: '', zip: '' };
+
+		// 	let incoming_message = {
+		// 		user_id: user_id,
+		// 		message: body.Body,
+		// 		from: body.From,
+		// 		to: body.To,
+		// 		sid: body.MessageSid,
+		// 		created_at: new Date().toISOString(),
+		// 		sent_at: new Date().toISOString(),
+		// 		updated_at: new Date().toISOString(),
+		// 		status: body.MessageStatus,
+		// 		direction: 'inbound',
+		// 		errorCode: '',
+		// 		errorMessage: '',
+		// 		lead_id: lead.lead_id,
+		// 		propertyAddress: propertyAddress as unknown as Json,
+		// 	};
+
+		// 	messages.push(incoming_message);
+		// });
 
 		return {
 			statusCode: 200,
 			body: 'success',
 			supabaseError,
 			user_id,
-			leads,
+			lead,
 			from: body.From,
 			insertedMessage,
 		};
