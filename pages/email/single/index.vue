05f942116940a1a4cbdb164465960e7d476b5994
Latent-Labs <luke@latent-labs.com>
2023-02-09 12:12:46 -0500

    publish changes

--- pages/email/single/index.vue
+++ pages/email/single/index.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="grid gap-4">
 		<h3 class="header">Send Single Email</h3>
-		<div class="grid gap-4 justify-center mx-auto">
+		<div class="grid gap-4 justify-center">
 			<div class="flex gap-4 justify-between">
 				<label for="">To:</label>
 				<input type="text" />
@@ -10,10 +10,11 @@
 				<label class="" for="">Subject:</label>
 				<input type="text" />
 			</div>
-			<div class="flex flex-col gap-4 justify-between w-full">
+			<div class="flex flex-col gap-4 justify-between">
 				<label class="text-center" for="">Message:</label>
-				<textarea class="w-80"></textarea>
+				<textarea class="w-80 sm:w-96"></textarea>
 			</div>
+			<button class="mx-auto">Send Email</button>
 		</div>
 	</div>
 </template>
