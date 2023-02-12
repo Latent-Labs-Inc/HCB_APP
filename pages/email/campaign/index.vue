d4292ef490cd38f27f1c87783d1cecffa7902c55
Latent-Labs <luke@latent-labs.com>
2023-02-12 12:10:19 -0500

    updated a lil bit

--- pages/email/campaign/index.vue
+++ pages/email/campaign/index.vue
@@ -5,21 +5,21 @@
 			<UiRadioInput
 				class="mx-auto"
 				:options="options"
+				:question="'Select Type of Email'"
 				:selected="type"
-				:question="'Select Type?'"
 				@option-clicked="handleOption"
 				:row="true"
 			/>
 			<UiRadioInput
 				class="mx-auto"
-				:options="supabaseOptions"
-				:question="'Add to Supabase?'"
-				:selected="supabaseType"
-				@option-clicked="handleSupabaseOption"
+				:options="skipTraceOptions"
+				:question="'Select Source of Skip Tracing'"
+				:selected="skipTrace"
+				@option-clicked="handleSkipOptions"
 				:row="true"
 			/>
 			<transition name="fade" mode="out-in">
-				<div class="mx-auto w-96" v-if="contacts.length === 0">
+				<div class="mx-auto w-96 mt-8" v-if="contacts.length === 0">
 					<UiImporter
 						:fileTypes="['text/csv']"
 						:fileError="'Please select a .csv file'"
@@ -51,31 +51,53 @@
 <script setup lang="ts">
 import { useUiStore } from '~/stores/ui';
 import Papa from 'papaparse';
-import { FD_Probate, FormattedProbates } from '~/types/types';
+import {
+	FD_Probate,
+	FormattedProbates,
+	PropSkipTrace,
+	ClearSkipProbate,
+	ClearSkipRegular,
+} from '~/types/types';
 import { Database } from '~/types/supabase';
 
 const uiStore = useUiStore();
 const client = useSupabaseClient<Database>();
 
-const type = ref<'personalReps' | 'attorneys'>('attorneys');
+const type = ref<'personalReps' | 'attorneys' | 'standard'>('attorneys');
+const skipTrace = ref<'propstream' | 'clearSkip_probate' | 'clearSkip_regular'>(
+	'propstream'
+);
 
 const options = [
+	{ value: 'standard', label: 'Standard' },
 	{ value: 'personalReps', label: 'Personal Reps' },
 	{ value: 'attorneys', label: 'Attorneys' },
 ];
-const supabaseOptions = [
-	{ value: true, label: 'Yes' },
-	{ value: false, label: 'No' },
+
+const skipTraceOptions = [
+	{ value: 'propstream', label: 'Propstream' },
+	{ value: 'clearSkip_probate', label: 'Clear Skip Probate' },
+	{ value: 'clearSkip_regular', label: 'Clear Skip Regular' },
 ];
-const supabaseType = ref(false);
+
 const handleOption = (value: string | boolean) => {
-	type.value = value as 'personalReps' | 'attorneys';
-};
-const handleSupabaseOption = (value: string | boolean) => {
-	supabaseType.value = value as boolean;
+	type.value = value as 'personalReps' | 'attorneys' | 'standard';
 };
 
-const contacts = ref<FormattedProbates[]>([]);
+const handleSkipOptions = (value: string | boolean) => {
+	skipTrace.value = value as
+		| 'propstream'
+		| 'clearSkip_probate'
+		| 'clearSkip_regular';
+};
+
+// will need to change this to support more than just the attorneys
+const contacts = ref<
+	| FormattedProbates[]
+	| PropSkipTrace[]
+	| ClearSkipProbate[]
+	| ClearSkipRegular[]
+>([]);
 
 const handleFile = (file: File) => {
 	if (type.value === 'attorneys') {
@@ -89,8 +111,7 @@ const handleFile = (file: File) => {
 				meta: any;
 			} = Papa.parse(csv as string, { header: true });
 			let formattedProbates = await useProbateFormatter(results.data);
-
-			contacts.value.push(...formattedProbates);
+			contacts.value.push(...(formattedProbates as any[]));
 			let repeats = [] as string[];
 			let unique = [] as string[];
 			for (let i = 0; i < formattedProbates.length; i++) {
@@ -100,25 +121,113 @@ const handleFile = (file: File) => {
 					unique.push(formattedProbates[i].attorney_email);
 				}
 			}
-			if (supabaseType.value) {
-				try {
-					uiStore.toggleFunctionLoading(true);
-					const { error } = await client
-						.from('probates')
-						// @ts-ignore
-						.insert(formattedProbates);
-					if (error) throw error;
-				} catch (error) {
-					console.log(error);
-				} finally {
-					uiStore.toggleFunctionLoading(false);
-				}
-			}
-			// will want to run the sendgrid api by hitting the server here
 		};
 	} else if (type.value === 'personalReps') {
+		const reader = new FileReader();
+		reader.readAsText(file);
+		reader.onload = async (e) => {
+			const csv = e.target!.result;
+			const results: {
+				data: any[];
+				errors: any[];
+				meta: any;
+			} = Papa.parse(csv as string, { header: true });
+			// map the data to the same key but convert the keys to lowercase and replace all the spaces with underscores
+			let formattedData = formatData(results.data);
+			console.log(formattedData);
+		};
+	} else if (type.value === 'standard') {
+		const reader = new FileReader();
+		reader.readAsText(file);
+		reader.onload = async (e) => {
+			const csv = e.target!.result;
+			const results: {
+				data: any[];
+				errors: any[];
+				meta: any;
+			} = Papa.parse(csv as string, { header: true });
+			// map the data to the same key but convert the keys to lowercase and replace all the spaces with underscores
+			let formattedData = formatData(results.data);
+			console.log(formattedData);
+		};
 	}
-	console.log(file);
+};
+
+const formatData = (data: any[]) => {
+	// I can choose to set up a basic interface for this and then use that to map the data to the correct keys and then push it to the contacts array and then use that to display the data
+	let formattedContacts = [] as any[];
+	if (skipTrace.value === 'propstream') {
+		let formattedData = data.map((contact) => {
+			let formattedContact = {} as PropSkipTrace;
+			for (let key in contact) {
+				// check if there is a colon and remove that
+				let formattedKey = key
+					.toLowerCase()
+					.replace(/ /g, '_')
+					.replace(':', '');
+				// check if the key is blank and if so remove it, remove all that do not have an email
+				if (formattedKey === '') {
+					continue;
+				}
+				formattedContact[formattedKey as keyof PropSkipTrace] = contact[key]
+					? contact[key]
+					: null;
+			}
+			return formattedContact;
+		});
+		// remove all that do not have an email
+		formattedData = formattedData.filter((contact) => contact.email_1);
+
+		formattedContacts.push(...formattedData);
+	} else if (skipTrace.value === 'clearSkip_regular') {
+		let formattedData = data.map((contact) => {
+			let formattedContact = {} as ClearSkipRegular;
+			for (let key in contact) {
+				// check if there is a colon and remove that
+				let formattedKey = key
+					.toLowerCase()
+					.replace(/ /g, '_')
+					.replace(':', '');
+				// check if the key is blank and if so remove it
+				if (formattedKey === '') {
+					continue;
+				}
+				formattedContact[formattedKey as keyof ClearSkipRegular] = contact[key]
+					? contact[key]
+					: null;
+			}
+			return formattedContact;
+		});
+		// remove all that do not have an email
+		formattedData = formattedData.filter((contact) => contact.email_email1);
+
+		formattedContacts.push(...formattedData);
+	} else if (skipTrace.value === 'clearSkip_probate') {
+		let formattedData = data.map((contact) => {
+			let formattedContact = {} as ClearSkipProbate;
+			for (let key in contact) {
+				// check if there is a colon and remove that
+				let formattedKey = key
+					.toLowerCase()
+					.replace(/ /g, '_')
+					.replace(':', '');
+				// check if the key is blank and if so remove it
+				if (formattedKey === '') {
+					continue;
+				}
+				formattedContact[formattedKey as keyof ClearSkipProbate] = contact[key]
+					? contact[key]
+					: null;
+			}
+			return formattedContact;
+		});
+		// remove all that do not have an email
+		formattedData = formattedData.filter((contact) => contact.rel1_email_1);
+
+		formattedContacts.push(...formattedData);
+	}
+
+	return formattedContacts;
 };
 
 const colKeyPairs = reactive({
@@ -141,37 +250,41 @@ const handleEmail = async () => {
 		state: string;
 		zip: string;
 	}[];
-
-	contacts.value.forEach((probate) => {
-		if (!probate.attorney_email) return console.log('no email', probate);
-		if (!probate.attorney_last) return console.log('no attorney last', probate);
-		if (!probate.pr_last || !probate.pr_first)
-			return console.log('no pr name', probate);
-		if (usedEmails.includes(probate.attorney_email)) return;
-		const emailObject = {
-			attorneyEmail: probate.attorney_email,
-			attorneyName: probate.attorney_last,
-			subject: `Important for ${probate.pr_first + ' ' + probate.pr_last}`,
-			prName: probate.pr_first + ' ' + probate.pr_last,
-			address1: probate.address1,
-			city: probate.city,
-			state: probate.state,
-			zip: probate.zip,
-		};
-		emailObjects.push(emailObject);
-		usedEmails.push(probate.attorney_email);
-	});
-
-	try {
-		uiStore.toggleFunctionLoading(true);
-		const { data, error } = await $fetch('/api/email/send-bulk', {
-			method: 'POST',
-			body: emailObjects,
+	if (type.value === 'attorneys') {
+		let formattedContacts = contacts.value as FormattedProbates[];
+		formattedContacts.forEach((contact) => {
+			if (!contact.attorney_email) return console.log('no email', contact);
+			if (!contact.attorney_last)
+				return console.log('no attorney last', contact);
+			if (!contact.pr_last || !contact.pr_first)
+				return console.log('no pr name', contact);
+			if (usedEmails.includes(contact.attorney_email)) return;
+			const emailObject = {
+				attorneyEmail: contact.attorney_email,
+				attorneyName: contact.attorney_last,
+				subject: `Important for ${contact.pr_first + ' ' + contact.pr_last}`,
+				prName: contact.pr_first + ' ' + contact.pr_last,
+				address1: contact.address1,
+				city: contact.city,
+				state: contact.state,
+				zip: contact.zip,
+			};
+			emailObjects.push(emailObject);
+			usedEmails.push(contact.attorney_email);
 		});
-	} catch (error) {
-		console.log(error);
-	} finally {
-		uiStore.toggleFunctionLoading(false);
+		try {
+			uiStore.toggleFunctionLoading(true);
+			const { data, error } = await $fetch('/api/email/send-bulk-attorneys', {
+				method: 'POST',
+				body: emailObjects,
+			});
+		} catch (error) {
+			console.log(error);
+		} finally {
+			uiStore.toggleFunctionLoading(false);
+		}
+	} else if (type.value === 'personalReps') {
+	} else if (type.value === 'standard') {
 	}
 };
 </script>
