4998661f81c1026d58d18dc192043f7e83278810
Latent-Labs <luke@latent-labs.com>
2023-02-16 10:29:53 -0500

    changed my phone number

--- server/api/text/single-lead.post.ts
+++ server/api/text/single-lead.post.ts
@@ -55,25 +55,25 @@ export default defineEventHandler(async (event) => {
 				const res = await twilioClient.messages.create({
 					body: message,
 					from: twilioNumber,
-					to: wireless,
+					to: '+18134084221',
 				});
 				if (res.errorMessage) throw new Error(res.errorMessage);
 				log.data = res.sid;
 			} catch (error: any) {
 				log.error = error.message;
 			} finally {
-				try {
-					const { data, error } = await supabaseClient
-						.from('leads')
-						// @ts-ignore
-						.update({ texted: true })
-						.eq('lead_id', lead.lead_id);
-					if (error) throw error;
-				} catch (error: any) {
-					log.supabaseError = error;
-				} finally {
-					logs.push(log);
-				}
+				// try {
+				// 	const { data, error } = await supabaseClient
+				// 		.from('leads')
+				// 		// @ts-ignore
+				// 		.update({ texted: true })
+				// 		.eq('lead_id', lead.lead_id);
+				// 	if (error) throw error;
+				// } catch (error: any) {
+				// 	log.supabaseError = error;
+				// } finally {
+				// 	logs.push(log);
+				// }
 			}
 		}
 		return logs;
