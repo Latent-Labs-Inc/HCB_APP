1beb1ac4e995461aa23f71c1cb4248733456c7c0
Latent-Labs <luke@latent-labs.com>
2023-02-22 12:38:29 -0500

    test to show apiKey

--- functions/cronJob.ts
+++ functions/cronJob.ts
@@ -4,11 +4,10 @@ import { Handler } from '@netlify/functions';
 
 const handler: Handler = async (event, context) => {
 	console.log('Cron job ran at ' + new Date().toLocaleString());
-
 	// 0. Load environment variables
 	dotenv.config();
 	const { API_KEY: apiKey } = process.env;
-	console.log('Running cron job');
+	console.log('apiKey', apiKey);
 	let appUrl = 'https://app.highestcashbuyer.com/api/public/flip-list-cron';
 	try {
 		const response = await axios.post(
