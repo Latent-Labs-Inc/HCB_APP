73ed6bf31b4e54070df7250f21a5c0a742a76bba
Latent-Labs <luke@latent-labs.com>
2023-02-22 12:37:54 -0500

    add handler to manage requests

--- functions/cronJob.ts
+++ functions/cronJob.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
 import dotenv from 'dotenv';
+import { Handler } from '@netlify/functions';
 
-const main = async () => {
+const handler: Handler = async (event, context) => {
+	console.log('Cron job ran at ' + new Date().toLocaleString());
+
 	// 0. Load environment variables
 	dotenv.config();
 	const { API_KEY: apiKey } = process.env;
@@ -19,9 +22,17 @@ const main = async () => {
 		);
 		console.log('data', response.data.data);
 		console.log('error', response.data.error);
+		return {
+			statusCode: 200,
+			body: JSON.stringify(response.data),
+		};
 	} catch (error) {
 		console.log(error);
+		return {
+			statusCode: 500,
+			body: JSON.stringify(error),
+		};
 	}
 };
 
-main();
+export { handler };
