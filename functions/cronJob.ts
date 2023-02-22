bb7d9be0c4054e75245b6dcb362ec85eaa1c4e67
Latent-Labs <luke@latent-labs.com>
2023-02-22 13:21:31 -0500

    updated cronApiKey

--- functions/cronJob.ts
+++ functions/cronJob.ts
@@ -6,7 +6,7 @@ const handler: Handler = async (event, context) => {
 	console.log('Cron job ran at ' + new Date().toLocaleString());
 	// 0. Load environment variables
 	dotenv.config();
-	const { API_KEY: apiKey } = process.env;
+	const { CRON_API_KEY: apiKey } = process.env;
 	console.log('apiKey', apiKey);
 	let appUrl = 'https://app.highestcashbuyer.com/api/public/flip-list-cron';
 	try {
