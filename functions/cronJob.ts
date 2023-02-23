62678f1b87ffd0a03eb7d9d866b444426bd866e5
Latent-Labs <luke@latent-labs.com>
2023-02-23 08:07:18 -0500

    changed a few things

--- functions/cronJob.ts
+++ functions/cronJob.ts
@@ -1,34 +1,44 @@
 import axios from 'axios';
 import dotenv from 'dotenv';
-import { Handler } from '@netlify/functions';
+import nodeCron from 'node-cron';
+import { Handler, HandlerEvent, HandlerContext } from '@netlify/functions';
 
 const handler: Handler = async (event, context) => {
-	console.log('Cron job ran at ' + new Date().toLocaleString());
-	// 0. Load environment variables
-	dotenv.config();
-	const { CRON_API_KEY: apiKey } = process.env;
-	console.log('apiKey', apiKey);
-	let appUrl = 'https://app.highestcashbuyer.com/api/public/flip-list-cron';
 	try {
-		const response = axios.post(
-			appUrl,
-			{
-				apiKey,
-			},
-			{
-				method: 'POST',
-				timeout: 30000,
-			}
-		);
-		return {
-			statusCode: 200,
-			body: JSON.stringify('Cron job ran successfully'),
-		};
+		// 1. Schedule cron job for every 15 minutes
+		nodeCron.schedule('*/15 * * * *', () => {
+			console.log('Cron job ran at ' + new Date().toLocaleString());
+			// 2. Call API
+			dotenv.config();
+			const { CRON_API_KEY: apiKey } = process.env;
+			console.log('apiKey', apiKey);
+			let appUrl = 'https://app.highestcashbuyer.com/api/public/flip-list-cron';
+
+			const response = axios.post(
+				appUrl,
+				{
+					apiKey,
+				},
+				{
+					method: 'POST',
+					timeout: 30000,
+				}
+			);
+			return {
+				statusCode: 200,
+				body: JSON.stringify('Cron job ran successfully'),
+			};
+		});
 	} catch (error) {
 		console.log(error);
 		return {
 			statusCode: 500,
 			body: JSON.stringify(error),
+		};
+	} finally {
+		return {
+			statusCode: 200,
+			body: JSON.stringify('Cron job ran successfully'),
 		};
 	}
 };
